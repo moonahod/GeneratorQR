efae8de22c2b8f1f218d0b075fd337870972ec00
moonahod <158854801+moonahod@users.noreply.github.com>
2024-10-20 17:08:31 +0300

    Update views.py

--- GeneratorQR/Olympia/views.py
+++ GeneratorQR/Olympia/views.py
@@ -4,9 +4,10 @@ from sysconfig import get_path
 
 from django.contrib.staticfiles.utils import get_files
 from django.core.handlers.wsgi import get_path_info
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+import qrcode
+#from .models import Draft
 from django.http import HttpResponse
-import json
 
 
 def home(response):
@@ -15,8 +16,31 @@ def home(response):
 def guide(response):
     return render(response, 'guide.html', {})
 
+#def qrdata(response):
+#    return render(response, 'qrdata.html', {})
+
 def qrdata(response):
     return render(response, 'qrdata.html', {})
+    #if response.method == 'POST':
+     #   data = response.POST.get('qrdata')
+     #   qrdraft = Draft(data=qrdata)
+    #return render(response, 'qrdata.html', {}), redirect('/code')
+    #if form.is_valid():
+    #    form.save()
+    #return redirect('../code/')
+    #return render(request, 'qrdata.html', {})
+
+def gencode(request):
+    if request.method == 'POST':
+        data = request.POST.get('qrdata')
+        #qr = qrcode.QRCode(
+
+        #)
+        #qr.make(fit=True)
+        #code = qr.make_image()
+        #code.save('1.png')
+
+    return render(request, 'qrcode.html', {})
 
 def code(response):
-    return render(response, 'qrcode.html', 
+    return render(response, 'qrcode.html', {})
