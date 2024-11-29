e8128add6a4c256747b58adfc59e2cfb3117d86b
moonahod <158854801+moonahod@users.noreply.github.com>
2024-11-29 21:20:06 +0300

    Update models.py

--- GeneratorQR/Olympia/models.py
+++ GeneratorQR/Olympia/models.py
@@ -1,16 +1,1 @@
-#from django.db import models
-#import qrcode
 
-#class CodeQR(models.Model):
-
-    #def __str__(self):
-        #return self.name
-
-    #data = request.POST.get('qrdata')
-    #filename = request.POST.get('filename')
-    #qr = qrcode.QRCode(error_correction=qrcode.constants.ERROR_CORRECT_H)
-    #qr.add_data(data)
-    #qr.make(fit=True)
-    #img = qr.make_image(fill_color="black", back_color="white")
-    #type(img)
-    #img.save('../GeneratorQR/Olympia/codes/' + filename + '.jpg')
