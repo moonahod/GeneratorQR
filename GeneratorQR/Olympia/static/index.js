c2ae003c06380a20a00ef345a5b536e1d70f1eee
moonahod <158854801+moonahod@users.noreply.github.com>
2025-02-09 19:54:14 +0000

    Update index.js

--- GeneratorQR/Olympia/static/index.js
+++ GeneratorQR/Olympia/static/index.js
@@ -1,22 +1,23 @@
-function Function1(x) {
-    document.getElementById('sidebar').classList.toggle('active');
-    x.classList.toggle("transform");
-}
+//var width = window.innerWidth;
+//console.log("Window width(from js): "+ width);
 
-/* function will be used to save previous data */
-function myFunction() {
-    document.getElementById("qrdata").defaultValue = "Test the new feature";
-}
+    function Function1(x) {
+        document.getElementById('sidebar').classList.toggle('active');
+        x.classList.toggle("transform");
+    }
 
-/* dropdown menu stuff */
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
-function myFunction2() {
-    document.getElementById("myDropdown").classList.toggle("show");
-}
+    //function myFunction() {
+    //    document.getElementById("qrdata").defaultValue = "Test the new feature";
+    //}
 
-// Close the dropdown if the user clicks outside of it
-window.onclick = function(event) {
+    /* dropdown menu stuff */
+    /* When the user clicks on the button,
+    toggle between hiding and showing the dropdown content */
+    function myFunction1() {
+        document.getElementById("myDropdown").classList.toggle("show");
+    }
+    // Close the dropdown if the user clicks outside of it
+    window.onclick = function(event) {
     if (!event.target.matches('.dropBtn')) {
         var dropdowns = document.getElementsByClassName("dropdown-content");
         var i;
@@ -24,27 +25,6 @@ window.onclick = function(event) {
             var openDropdown = dropdowns[i];
             if (openDropdown.classList.contains('show')) {
                 openDropdown.classList.remove('show');
-            }
-        }
-    }
-}
-
-/* second dropdown menu stuff */
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
-function myFunction3() {
-document.getElementById("myDropdown2").classList.toggle("show2");
-}
-
-// Close the dropdown if the user clicks outside of it
-window.onclick = function(event) {
-    if (!event.target.matches('.dropBtn2')) {
-        var dropdowns = document.getElementsByClassName("dropdown-content2");
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show2')) {
-                openDropdown.classList.remove('show2');
             }
         }
     }
