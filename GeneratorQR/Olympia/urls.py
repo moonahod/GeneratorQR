4521dfb7e80be59b18b0d062556094aeecb07f4c
moonahod <158854801+moonahod@users.noreply.github.com>
2025-02-15 20:57:08 +0300

    Update urls.py

--- GeneratorQR/Olympia/urls.py
+++ GeneratorQR/Olympia/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
-    path('/', views.home),
-    #path('', views.code)
+    path('', views.lang),
+    #path('/', views.lang),
+    path('en/', views.home_en),
+    path('by/', views.home_by),
+    #path('/en/', views.home)
+    #path('code/', views.code)
 ]
