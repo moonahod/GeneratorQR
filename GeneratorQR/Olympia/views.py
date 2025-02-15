e7a3c1a006f842478c9c7472381ba5c3a3b9a04d
moonahod <158854801+moonahod@users.noreply.github.com>
2025-02-15 20:54:18 +0300

    Update views.py

--- GeneratorQR/Olympia/views.py
+++ GeneratorQR/Olympia/views.py
@@ -1,11 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 import qrcode
 
+def lang(request):
+    return render(request, 'homepage.html')
+    #render(request, 'homepage.html')
+    #language = request.POST.get('language')
 
-def home(request):
+    #if request.method == 'POST' and language == "EN":
+        #return redirect(home_en)
+    #elif request.method == 'POST' and language == "BY":
+        #return redirect(home_by)
+
+
+def home_en(request):
     if request.method == 'POST':
-
-        print('Hey, new code coming!')
+        print('eng version code gen')
 
         data = request.POST.get('qrdata')
         print('data to code: ', data)
@@ -27,13 +36,76 @@ def home(request):
         if not fileFormat:
             fileFormat = '.jpg'
 
-        #win = fpmi.gui.getWindow('MyWindow')
-        #width = win.size.width
-        #print('window width: ', width)
-        #if width <= 420:
-        #    mobile = True
-        #else:
-        #    mobile = False
+        # Error Correction Setting
+        if errorCorrection == 'L':
+            qr = qrcode.QRCode(error_correction=qrcode.constants.ERROR_CORRECT_L)
+        elif errorCorrection == 'M':
+            qr = qrcode.QRCode(error_correction=qrcode.constants.ERROR_CORRECT_M)
+        elif errorCorrection == 'Q':
+            qr = qrcode.QRCode(error_correction=qrcode.constants.ERROR_CORRECT_Q)
+        elif errorCorrection == 'H':
+            qr = qrcode.QRCode(error_correction=qrcode.constants.ERROR_CORRECT_H)
+
+        qr.add_data(data)
+        qr.make(fit=True)
+
+        # Color Playground
+        img = qr.make_image(fill_color=patternColor, back_color=bgColor)
+        if not patternColor:
+            img = qr.make_image(fill_color="black", back_color=bgColor)
+        elif not bgColor:
+            img = qr.make_image(fill_color=patternColor, back_color="#f2eddf")
+        else:
+            img = qr.make_image(fill_color="#000", back_color="#f2eddf")
+
+        type(img)
+
+        name = (filename + fileFormat)
+        finalcode = img.save('../GeneratorQR/Olympia/static/codes/' + name)
+        path = ('/static/codes/' + name)
+
+        if len(data) > 80:
+            huge_code_check = True
+        else:
+            huge_code_check = False
+
+        context = {
+            #'code1': img,
+            'code1': path,
+            'data_check': len(path) > 14,
+            'huge_code_check' : huge_code_check,
+            #'mobile' : mobile,
+            #'last_data': data,
+            #'last_filename': filename,
+            #'last_fileFormat': fileFormat,
+        }
+        return render(request, 'homepage-en.html', context)
+    else:
+        return render(request, 'homepage-en.html')
+
+def home_by(request):
+    if request.method == 'POST':
+        print('bel version code gen')
+
+        data = request.POST.get('qrdata')
+        print('data to code: ', data)
+
+        filename = request.POST.get('filename')
+        print('filename: ', filename)
+
+        errorCorrection = request.POST.get('EC')
+        print('EC level: ', errorCorrection)
+
+        patternColor = request.POST.get('color')
+        print('pattern color: ', patternColor)
+
+        bgColor = request.POST.get('bg-color')
+        print('background color: ', bgColor)
+
+        fileFormat = request.POST.get('fileFormat')
+        print('file format: ', fileFormat)
+        if not fileFormat:
+            fileFormat = '.jpg'
 
         # Error Correction Setting
         if errorCorrection == 'L':
@@ -54,7 +126,7 @@ def home(request):
             img = qr.make_image(fill_color="black", back_color=bgColor)
         elif not bgColor:
             img = qr.make_image(fill_color=patternColor, back_color="white")
-        elif not patternColor and bgColor:
+        else:
             img = qr.make_image(fill_color="#000", back_color="#f2eddf")
 
         type(img)
@@ -68,9 +140,6 @@ def home(request):
         else:
             huge_code_check = False
 
-        #if windowWidth <= 650:
-           # mobileVersion = True
-
         context = {
             #'code1': img,
             'code1': path,
@@ -81,6 +150,6 @@ def home(request):
             #'last_filename': filename,
             #'last_fileFormat': fileFormat,
         }
-        return render(request, 'homepage.html', context)
+        return render(request, 'homepage-by.html', context)
     else:
-        return render(request, 'homepage.html')
+        return render(request, 'homepage-by.html')
