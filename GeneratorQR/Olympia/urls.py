7be20a153595ad8d39f14076ad6c91bdc82a0b96
moonahod <158854801+moonahod@users.noreply.github.com>
2025-02-16 22:54:27 +0300

    Update urls.py

--- GeneratorQR/Olympia/urls.py
+++ GeneratorQR/Olympia/urls.py
@@ -3,9 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.lang),
-    #path('/', views.lang),
+    path('/', views.lang),
     path('en/', views.home_en),
     path('by/', views.home_by),
-    #path('/en/', views.home)
     #path('code/', views.code)
 ]
